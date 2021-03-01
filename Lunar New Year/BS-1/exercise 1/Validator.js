5d828c38ff2f69ddd60e71ab11fdc80ed799c2f7
Giang <truonggianhpham1@gmail.com>
2021-03-01 14:52:00 +0700

    Signed-off-by: Giang <truonggianhpham1@gmail.com>

---
+++ Lunar New Year/BS-1/exercise 1/Validator.js
@@ -0,0 +1,29 @@
+﻿//ham validator
+function Validator(options){
+    date='';
+        gender='';
+        firstName = $("#js-first-name").val();
+        lastName = $("#js-last-name").val();
+        gender =$( "input:checked" ).val()
+        date = $("#js-date").val();
+        if(firstName && lastName){
+            $('#result').html(firstName+'<br>'+lastName+'<br>'+date+'<br>'+gender);
+        }else {
+            $('#result').html('');
+        }
+
+        return false;
+    }
+
+//định nghĩa rules
+
+$("#reg-form").validate({
+        rules: {
+            "first-name": "required",
+            "last-name": "required",
+        },
+        messages: {
+            "first-name": "<div class='text-danger'>Trường này bắt buộc nhập</div>",
+            "last-name": "<div class='text-danger'>Trường này bắt buộc nhập</div>",
+        }
+    });
